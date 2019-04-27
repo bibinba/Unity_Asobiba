2826ac574c5a053303c0dcfeb061919881392859
bibinba <bibi@gma.com>
2019-04-27 20:07:22 +0900

    配列書いた

--- Assets/hairetu.cs
+++ Assets/hairetu.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hairetu : MonoBehaviour
 {
+    public Text arrayText;
+    private string[] stringArray = new string[10];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        arrayText.text = "";
+        for (int i=0; i<stringArray.Length; i++)
+        {
+            stringArray[i] = Random.Range(1, 100).ToString();
+            arrayText.text = arrayText.text + stringArray[i] + ",";
+        }
     }
 
     // Update is called once per frame
